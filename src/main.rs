d5778da5fe760c23a0c7132115173eac6e862d15
Rage Fade <ragef6@tutanota.de>
2026-02-01 16:07:58 -0500

    Update main.rs to include environment variable logic.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,33 @@
 mod encrypt;
 use clap::{arg, command, value_parser};
 use encrypt::encrypt_file;
-use std::{path::PathBuf, process::ExitCode};
+use shellexpand::full;
+use std::{path::PathBuf, process::ExitCode, str::FromStr};
 fn main() -> ExitCode {
     println!("Welcome to OxiVault, the blazing-fast password manager!");
     let matches = command!()
         .arg(
             arg!([file] "OxiVault file to operate on")
                 .required(false)
-                .value_parser(value_parser!(PathBuf)),
+                .value_parser(value_parser!(String)),
         )
         .get_matches();
-    let Some(vaultfile) = matches.get_one::<PathBuf>("file") else {
+    let Some(vaultfile) = matches.get_one::<String>("file") else {
         return ExitCode::FAILURE;
+    };
+    let vaultfile = &match full(vaultfile) {
+        Ok(p) => p,
+        Err(_) => {
+            eprintln!("Failure: Failed to expand environment variables.");
+            return ExitCode::FAILURE;
+        }
+    };
+    let vaultfile = match PathBuf::from_str(vaultfile) {
+        Ok(p) => p,
+        Err(_) => {
+            eprintln!("Failed to parse input to path.");
+            return ExitCode::FAILURE;
+        }
     };
     if !vaultfile.exists() {
         return ExitCode::FAILURE;
