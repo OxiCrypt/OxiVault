b0dbdc08c1a71324448877d3ea2b485b2fce3593
Rage Fade <ragef6@tutanota.de>
2026-02-04 18:44:50 -0500

    AAD use + minor bug patches relating to seek Also Decryption doesn't write to disk anymore

--- src/encrypt/mod.rs
+++ src/encrypt/mod.rs
@@ -5,7 +5,7 @@ use std::{
 mod passwd;
 use chacha20poly1305::{
     XChaCha20Poly1305, XNonce,
-    aead::{Aead, AeadCore, KeyInit, OsRng},
+    aead::{Aead, AeadCore, KeyInit, OsRng, Payload},
 };
 use passwd::getkey;
 use rand::Rng;
@@ -23,23 +23,30 @@ pub fn encrypt_file(plaintext: &mut Vec<u8>, file: &mut File) -> Result<(), Erro
         }
     };
     let nonce = XChaCha20Poly1305::generate_nonce(OsRng);
-    let ciphertext = match cipher.encrypt(&nonce, &plaintext[..]) {
+    let mut aad = Vec::new();
+    aad.extend_from_slice(&MAGIC_BYTES);
+    aad.extend_from_slice(nonce.as_slice());
+    aad.extend_from_slice(&salt);
+    let ciphertext = match cipher.encrypt(
+        &nonce,
+        Payload {
+            msg: &plaintext[..],
+            aad: &aad[..],
+        },
+    ) {
         Ok(c) => c,
         Err(_) => return Err(Error::new(std::io::ErrorKind::Other, "Error in encryption")),
     };
     drop(cipher);
     drop(key);
-    file.seek(SeekFrom::Start(0))?;
+    file.set_len(0)?;
     file.write_all(&MAGIC_BYTES)?;
-    file.seek(SeekFrom::Current(8))?;
     file.write_all(nonce.as_slice())?;
-    file.seek(SeekFrom::Current(24))?;
     file.write_all(&salt)?;
-    file.seek(SeekFrom::Current(16))?;
     file.write_all(&ciphertext[..])?;
     Ok(())
 }
-pub fn decrypt_file(ciphertext: &mut Vec<u8>, file: &mut File) -> Result<Vec<u8>, Error> {
+pub fn decrypt_file(ciphertext: &mut Vec<u8>) -> Result<Vec<u8>, Error> {
     if !ciphertext.starts_with(&MAGIC_BYTES) {
         return Err(Error::new(
             std::io::ErrorKind::InvalidData,
@@ -56,15 +63,21 @@ pub fn decrypt_file(ciphertext: &mut Vec<u8>, file: &mut File) -> Result<Vec<u8>
             panic!("Creating Cipher Failed.");
         }
     };
-    let plaintext = match cipher.decrypt(nonce, &ciphertext[48..]) {
+    let mut aad = Vec::new();
+    aad.extend_from_slice(&MAGIC_BYTES);
+    aad.extend_from_slice(nonce.as_slice());
+    aad.extend_from_slice(&salt);
+    let plaintext = match cipher.decrypt(
+        nonce,
+        Payload {
+            msg: ciphertext,
+            aad: &aad[..],
+        },
+    ) {
         Ok(c) => c,
         Err(_) => return Err(Error::new(std::io::ErrorKind::Other, "Error in decryption")),
     };
     drop(cipher);
     drop(key);
-    file.seek(SeekFrom::Start(0))?;
-    file.write_all(&ciphertext[..])?;
-    file.seek(SeekFrom::End(0))?;
-    file.write_all(nonce.as_slice())?;
     Ok(plaintext)
 }
