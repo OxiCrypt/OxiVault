6d64e7a482b4690c8af72968bee970604c44bf89
Rage Fade <ragef6@tutanota.de>
2026-02-07 11:57:04 -0500

    Fix passwd.rs to correct key size

--- src/encrypt/passwd.rs
+++ src/encrypt/passwd.rs
@@ -1,7 +1,7 @@
 use argon2::Argon2;
 use rpassword::prompt_password;
 use zeroize::Zeroize;
-pub struct Key(pub [u8; 16]);
+pub struct Key(pub [u8; 32]);
 impl Zeroize for Key {
     fn zeroize(&mut self) {
         self.0.zeroize();
@@ -21,7 +21,7 @@ pub fn getkey(salt: &[u8]) -> Key {
     };
     let mut passbytes = pass.as_bytes().to_owned();
     pass.zeroize();
-    let mut outkey = Key([0u8; 16]);
+    let mut outkey = Key([0u8; 32]);
     match Argon2::default().hash_password_into(&passbytes, salt, &mut outkey.0) {
         Ok(_) => (),
         Err(_) => {
