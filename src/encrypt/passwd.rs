7dd1e3cfcd2ae15092afc7a17b7a3eec2998570a
Rage Fade <ragef6@tutanota.de>
2026-02-12 14:20:13 -0500

    Prepare passwd.rs for TESTS

--- src/encrypt/passwd.rs
+++ src/encrypt/passwd.rs
@@ -31,8 +31,11 @@ pub fn getkey(salt: &[u8], params: Params) -> Key {
     let Ok(mut pass) = prompt_password("Enter your password.") else {
         panic!("Error prompting password. Please try again.")
     };
-    let mut passbytes = pass.as_bytes().to_owned();
-    pass.zeroize();
+    derivekey(salt, params, &mut pass)
+}
+fn derivekey(salt: &[u8], params: Params, pass: &mut String) -> Key {
+    let mut passbytes = (*pass).as_bytes().to_owned();
+    (*pass).zeroize();
     let mut outkey = Key::from_slice(&[0u8; 32]);
     let hasher = Argon2::new(Algorithm::Argon2id, Version::V0x13, params);
     let Ok(()) = hasher.hash_password_into(&passbytes, salt, outkey.as_mut_slice()) else {
