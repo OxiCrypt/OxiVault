19f343f8d29fb60954927c1e9b9df99738c26bed
Rage Fade <ragef6@tutanota.de>
2026-02-18 18:06:54 -0500

    Add interactive encrypt/decrypt prompt
    Prompt user for encryption or decryption mode with basic error handling.
    
    Add retry logic for stdin failures.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 mod encrypt;
 use clap::Parser;
 use shellexpand::full;
-use std::{path::PathBuf, process::ExitCode, str::FromStr};
+use std::{io, path::PathBuf, process::ExitCode, str::FromStr};
 // TODO: Implement actual handling
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -14,6 +14,7 @@ fn main() -> ExitCode {
     println!("Welcome to OxiVault, the blazing-fast file encryptor!");
     let args = Oxivault::parse();
     let vaultfile = args.file;
+    let mut errcount = 0;
     let vaultfile = &if let Ok(p) = full(&vaultfile) {
         PathBuf::from_str(p.as_ref())
     } else {
@@ -25,7 +26,30 @@ fn main() -> ExitCode {
         eprintln!("Error: File does not exist!");
         return ExitCode::FAILURE;
     }
-    println!("Opening vault {}", vaultfile.display());
-    println!("Sorry, OxiVault is unfinished");
+    println!("Would you like to encrypt or decrypt today?");
+    let stdin = io::stdin();
+    let ecdc: u8 = loop {
+        let mut ecdc = String::new();
+        let Ok(_) = stdin.read_line(&mut ecdc) else {
+            if errcount >= 3 {
+                panic!("Stdin borked");
+            } else {
+                eprintln!("Error gathering input, please try again.");
+                eprintln!(
+                    "If the issue persists, this program will exit.\nTry stty sane from there."
+                );
+                errcount += 1;
+                continue;
+            }
+        };
+
+        ecdc = ecdc.to_lowercase();
+        if ecdc.starts_with('e') {
+            break 1;
+        } else if ecdc.starts_with('d') {
+            break 0;
+        }
+        println!("Please enter a valid mode.");
+    };
     ExitCode::SUCCESS
 }
